f38aace47192b0ad405d9ac383bf9b8411a1689a
David Barroso <dbarrosop@dravetech.com>
2023-04-09 08:20:40 +0200

    asd

--- functions/echo.ts
+++ functions/echo.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express'
 
 export default (req: Request, res: Response) => {
-    res.status(200).json(
-        {
-            headers: req.headers,
-            query: req.query,
-        },
+    res.header('Content-Type', 'blah').
+        header('X-Test', 'test').
+        status(200).
+        send(JSON.stringify(
+            {
+                headers: req.headers,
+                query: req.query,
+            },
+        ),
     )
 }
