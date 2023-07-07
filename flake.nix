8599eb33e460237523f55ca70cce3679bbd0a723
David Barroso <dbarrosop@dravetech.com>
2023-07-07 08:47:40 +0200

    asdasd

--- flake.nix
+++ flake.nix
@@ -8,41 +8,19 @@
   outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        version = "v1.1.0";
+        version = "v1.3.0";
         dist = {
           aarch64-darwin = rec {
             url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-darwin-arm64.tar.gz";
-            sha256 = "sha256-tF40CEkA357yzg2Gmc9ubjHJ5FlI9qQTdVdWNY/+f+Y=";
+            sha256 = "10dzdbpmw28rsqxglq8jwswawdsqp3pqibq725vyv6b9sxz5x0x6";
           };
           x86_64-linux = rec {
             url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-linux-amd64.tar.gz";
-            sha256 = "sha256-KLv06dI7A+5KGJ5F8xM1qC+oqHRmJ4kMaifLvaTFqak=";
+            sha256 = "12ry86f0rkck3rgrq5vhk4pqqbb1h70n0zvnd67kwxzlx5ibb95s";
           };
         };
         overlays = [
           (final: prev: rec {
-            hasura-cli = prev.hasura-cli.override {
-              buildGoModule = args: final.buildGoModule (args // rec {
-                version = "2.15.2";
-                src = final.fetchFromGitHub {
-                  owner = "hasura";
-                  repo = "graphql-engine";
-                  rev = "v${version}";
-                  sha256 = "sha256-q5Pk8K6WlkPMsegdKAqXXTFtPlN65Y1luoAsvjoW+20=";
-                };
-
-                ldflags = [
-                  "-X github.com/hasura/graphql-engine/cli/v2/version.BuildVersion=${version}"
-                  "-X github.com/hasura/graphql-engine/cli/v2/plugins.IndexBranchRef=master"
-                  "-s"
-                  "-w"
-                  "-extldflags"
-                  "\"-static\""
-                ];
-                vendorSha256 = "sha256-vZKPVQ/FTHnEBsRI5jOT6qm7noGuGukWpmrF8fK0Mgs=";
-              });
-            };
-
             nhost = final.stdenvNoCC.mkDerivation rec {
               pname = "nhost-cli";
               inherit version;
@@ -65,8 +43,6 @@
 
                 mkdir -p $out/bin
                 mv cli $out/bin/nhost
-
-                wrapProgram $out/bin/nhost --set HASURACLI ${final.hasura-cli}/bin/hasura
 
                 installShellCompletion --cmd nhost \
                   --bash <($out/bin/nhost completion bash) \
