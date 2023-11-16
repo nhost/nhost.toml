da5288028cc301bd87a2acf26dc05b22a34c44e2
David Barroso <dbarrosop@dravetech.com>
2023-11-16 08:41:44 +0100

    asd

--- functions/gzip.ts
+++ functions/gzip.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express'
 import { gzip } from 'zlib';
+import { promisify } from 'util';
+
+const gzipAsync = promisify(gzip);
 
 export default (req: Request, res: Response) => {
-    const body = "here is a body"
+    let body = "here is a body"
 
     res.setHeader('Content-Type', 'text/plain')
 
     if (req.headers['accept-encoding']?.includes('gzip')) {
-        res.setHeader('Content-Encoding', 'gzip')
-        gzip(body, (_, result) => res.end(result))
-        return
+        res.setHeader('Content-Encoding', 'gzip');
+        try {
+            body = await gzipAsync(body);
+        } catch (error) {
+            console.error('Error compressing body:', error);
+        }
     }
 
     res.status(200).send(body)
