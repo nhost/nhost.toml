5b5a8dc6126d0575f9e6d5d445e81bb580b63025
David Barroso <dbarrosop@dravetech.com>
2023-11-16 09:03:12 +0100

    asd

--- functions/gzip.ts
+++ functions/gzip.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from 'express'
-import { gzip } from 'zlib';
+import { gzipSync } from 'zlib';
 
 export default (req: Request, res: Response) => {
     const body = "here is a body"
-
-    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Content-Type', 'text/plain');
 
     if (req.headers['accept-encoding']?.includes('gzip')) {
         res.setHeader('Content-Encoding', 'gzip');
-        gzip(body, (_, result) => res.status(200).end(result))
+        res.status(200).send(gzipSync(body))
     } else {
-       res.status(200).end(body)
+       res.status(200).send(body)
     }
 }
