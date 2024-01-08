0563f6a81b8d2703ca5cb19ad6a22212f0e441dc
David Barroso <dbarrosop@dravetech.com>
2024-01-08 16:39:17 +0100

    asd

---
+++ nhost/migrations/default/1704727897714_configure_animals_auto-embeddings/up.sql
@@ -0,0 +1,4 @@
+INSERT INTO graphite.auto_embeddings_configuration
+  (name, schema_name, table_name, column_name, query, mutation)
+VALUES
+  ('animals', 'public', 'animals', 'embeddings', 'query ...', 'mutation ...');
